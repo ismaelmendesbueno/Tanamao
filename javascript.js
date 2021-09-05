4278350759a1a3743bbfacc9aa954d9676478566
ismaelmendesbueno <yasuobestgm@gmail.com>
2021-09-05 20:37:06 -0300

    Conclusao do toogle

--- javascript.js
+++ javascript.js
@@ -3,16 +3,17 @@
 
     let btn=doc.querySelector("#btn");
     let menu=doc.querySelector(".menuu");
+   
     
 
     function toggle(event) {
         if(menu.classList.contains("show")){
             menu.classList.remove("show");
-            btn.setAttribute("src","button.png");
+            
            
         }else{
             menu.classList.add("show");
-            btn.setAttribute("src","close.png");
+            
             
         }
     }
