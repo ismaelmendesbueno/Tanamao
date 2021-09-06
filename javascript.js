92de0538928bdd90420125ab0ba799577b7cc5b6
ismaelmendesbueno <yasuobestgm@gmail.com>
2021-09-06 18:56:34 -0300

    ajustes

--- javascript.js
+++ javascript.js
@@ -21,3 +21,50 @@
 })(window, document);
 
 
+
+(function readyJS(win,doc){
+    'use strict';
+
+    let btn=doc.querySelector("#service");
+    let menu=doc.querySelector(".entregas");
+   
+    
+
+    function toggle(event) {
+        if(menu.classList.contains("active")){
+            menu.classList.remove("active");
+            
+           
+        }else{
+            menu.classList.add("active");
+            
+            
+        }
+    }
+    btn.addEventListener("click",toggle,false);
+})(window, document);
+
+
+(function readyJS(win,doc){
+    'use strict';
+
+    let btn=doc.querySelector("#services");
+    let menu=doc.querySelector(".entrega");
+   
+    
+
+    function toggle(event) {
+        if(menu.classList.contains("active")){
+            menu.classList.remove("active");
+            
+           
+        }else{
+            menu.classList.add("active");
+            
+            
+        }
+    }
+    btn.addEventListener("click",toggle,false);
+})(window, document);
+
+
